a65a79e741a6a3834aef8e288f940bed7e498873
Simone Di Leo <Simone Di Leo@DESKTOP-R0ICSHP.lan>
2017-04-12 13:40:38 +0200

    progetto soluzione secondo esonero piu repositoryCRUD

---
+++ correzioneSecondoEsonero/src/main/EsercitazioneMain.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.persistence.*;
+
+import model.Artista;
+import persistence.CrudRepository;
+import persistence.jpa.CrudRepositoryJPA;
+
+public class EsercitazioneMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("correzione-unit");
+		EntityManager em = emf.createEntityManager();
+
+		Artista a = new Artista();
+		a.setNome("Pablo");
+		
+		CrudRepository<Artista> crudRepository = new CrudRepositoryJPA<Artista>(em, Artista.class);
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+//		artistaRepository.save(a);
+		
+		
+		tx.commit();
+		
+		em.close();
+		emf.close();
+	}
+}
